3ef8e14bb95581cdcdf6ff68da279ade99a9e485
Imam Busyiri <32695525+imambys@users.noreply.github.com>
2019-09-19 14:34:40 +0700

    Add files via upload

---
+++ PIR/PIR.ino
@@ -0,0 +1,22 @@
+#define pir  D8
+
+int Spir, Dpir;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pir, INPUT_PULLUP);      // GPIO 5 Pir Sensor
+  Serial.begin(9600);
+}
+
+void loop() {
+  Spir = digitalRead(pir);  // Sensor pir
+  if (Spir == 1) {
+    Dpir = 1;
+    Serial.print("Read digital input: ");
+    Serial.println(Dpir);
+  } else {
+    Dpir = 0;
+    Serial.print("Read digital input: ");
+    Serial.println(Dpir);
+  }
+  delay(1000);
+}
